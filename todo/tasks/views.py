4bdf5e798a41ee152696da5ddbf3a779d6d38323
Amit-Mahi <Amythcode@gmail.com>
2024-02-10 23:17:12 +0530

    commit 2

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,7 +1,31 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import *
+from .forms import *
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello there, This is Home Page")
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {
+        'tasks': tasks,
+        'form': form
+    }
+    return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    return render(request, "tasks/update_task.html")
